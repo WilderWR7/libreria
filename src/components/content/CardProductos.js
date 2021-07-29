4d1867349fdd09831d1ede1bf3ba338469967ae4
WilderWR7 <71362165+WilderWR7@users.noreply.github.com>
2021-07-29 16:33:34 -0400

    add /libreria

--- src/components/content/CardProductos.js
+++ src/components/content/CardProductos.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
 export const CardProductos = ( { products } ) => {
-    // console.log(products)
     return (
         <div className="product card-background">
             <div className="grid-inner">
                 <div className="product-image">
-                    <Link to = {`/product/${products.id}`}><img src= {`/libreria/assets/img/libreria/${products.categoria}/${products.id}.jpg`} alt={`${products.id}`} /></Link>
+                    <Link to = {`/libreria/product/${products.id}`}><img src= {`/libreria/assets/img/libreria/${products.categoria}/${products.id}.jpg`} alt={`${products.id}`} /></Link>
                 </div>
                 <div className="product-desc pl-2">
-                    <div className="product-title"><h3 className = 'titulo-producto'><Link to = {`/product/${products.id}`} > {products.id} </Link></h3></div>
+                    <div className="product-title"><h3 className = 'titulo-producto'><Link to = {`/libreria/product/${products.id}`} > {products.id} </Link></h3></div>
                     <div className="product-price"><ins>Bs. {products.precio}</ins></div>
                     <div className="product-rating">
                         <i className="icon-star3"></i>
