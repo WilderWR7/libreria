64c49d8609d818e296e91adde3b80b9f1e8a35ab
WilderWR7 <71362165+WilderWR7@users.noreply.github.com>
2021-07-29 16:40:48 -0400

    add libreria

--- src/components/content/Information.js
+++ src/components/content/Information.js
@@ -8,7 +8,7 @@ export const Information = () => {
 						<div className="row col-mb-50">
 							<div className="col-sm-6 col-lg-3 pb-4">
 								<div className="feature-box fbox-plain fbox-dark fbox-sm">
-									<Link to = '/' className="social-icon si-facebook" data-toggle="tooltip" data-placement="top" title="Facebook">
+									<Link to = '/libreria' className="social-icon si-facebook" data-toggle="tooltip" data-placement="top" title="Facebook">
 										<i className="icon-facebook"></i>
 										<i className="icon-facebook"></i>
 									</Link>
@@ -20,7 +20,7 @@ export const Information = () => {
 							</div>
 							<div className="col-sm-6 col-lg-3 pb-4">
 								<div className="feature-box fbox-plain fbox-dark fbox-sm">		
-									<Link to = '/' className="social-icon si-gplus" data-toggle="tooltip" data-placement="top" title="Google+">
+									<Link to = '/libreria' className="social-icon si-gplus" data-toggle="tooltip" data-placement="top" title="Google+">
 										<i className="icon-gplus"></i>
 										<i className="icon-gplus"></i>
 									</Link>
@@ -32,7 +32,7 @@ export const Information = () => {
 							</div>
 							<div className="col-sm-6 col-lg-3 pb-4">
 								<div className="feature-box fbox-plain fbox-dark fbox-sm">		
-									<Link to = '/' className="social-icon si-call" data-toggle="tooltip" data-placement="top" title="Call">
+									<Link to = '/libreria' className="social-icon si-call" data-toggle="tooltip" data-placement="top" title="Call">
 										<i className="icon-call"></i>
 										<i className="icon-call"></i>
 									</Link>
@@ -44,7 +44,7 @@ export const Information = () => {
 							</div>
 							<div className="col-sm-6 col-lg-3 pb-4">
 								<div className="feature-box fbox-plain fbox-dark fbox-sm">		
-									<Link to = '/' className="social-icon si-paypal" data-toggle="tooltip" data-placement="top" title="PayPal">
+									<Link to = '/libreria' className="social-icon si-paypal" data-toggle="tooltip" data-placement="top" title="PayPal">
 										<i className="icon-paypal"></i>
 										<i className="icon-paypal"></i>
 									</Link>
