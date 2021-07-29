bf3dcafbae8b065c7ad4a254887511d7e7590192
WilderWR7 <71362165+WilderWR7@users.noreply.github.com>
2021-07-29 16:39:09 -0400

    add Link

--- src/components/content/Connect.js
+++ src/components/content/Connect.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export const Connect = () => {
     return (
         <div className="row justify-content-center mb-0">
@@ -9,25 +9,25 @@ export const Connect = () => {
 								<h4>Connect with Us</h4>
 							</div>
 								<div className = 'centrar'>
-									<a href="#" className="social-icon si-facebook" data-toggle="tooltip" data-placement="top" title="Facebook">
+									<Link to = '/libreria' className="social-icon si-facebook" data-toggle="tooltip" data-placement="top" title="Facebook">
 										<i className="icon-facebook"></i>
 										<i className="icon-facebook"></i>
-									</a>
+									</Link>
 		
-									<a href="#" className="social-icon si-paypal" data-toggle="tooltip" data-placement="top" title="PayPal">
+									<Link to = '/libreria' className="social-icon si-paypal" data-toggle="tooltip" data-placement="top" title="PayPal">
 										<i className="icon-paypal"></i>
 										<i className="icon-paypal"></i>
-									</a>
+									</Link>
 		
-									<a href="#" className="social-icon si-gplus" data-toggle="tooltip" data-placement="top" title="Google+">
+									<Link to = '/libreria' className="social-icon si-gplus" data-toggle="tooltip" data-placement="top" title="Google+">
 										<i className="icon-gplus"></i>
 										<i className="icon-gplus"></i>
-									</a>
+									</Link>
 		
-									<a href="#" className="social-icon si-call" data-toggle="tooltip" data-placement="top" title="Call">
+									<Link to = '/libreria' className="social-icon si-call" data-toggle="tooltip" data-placement="top" title="Call">
 										<i className="icon-call"></i>
 										<i className="icon-call"></i>
-									</a>
+									</Link>
 								</div>
 						</div>
 					</div>
